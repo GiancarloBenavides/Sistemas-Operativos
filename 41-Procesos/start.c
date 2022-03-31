623ff3185043f7b2dbe418d3132ec4424b9c7e1c
Giancarlo <giancarlobenavides@hotmail.com>
2022-03-30 22:24:12 -0500

    gs-06

--- 41-Procesos/start.c
+++ 41-Procesos/start.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 
 /* función principal */
@@ -9,20 +10,22 @@ int main() {
 
     /* crea un nuevo proceso */
     pid = fork();
+    
     if (pid < 0) {
         /* error */
         fprintf(stderr, "No he podido crear el proceso hijo \n");
         exit(-1);
     }
-    if (pid == 0) 
+
+    if (pid == 0) {
         /* proceso hijo */
-        printf("Child Complete");
+        printf("Soy un proceso hijo, mi PID es %d y mi PPID es %d \n", getpid(), getppid());
         execlp("/bin/ls", "ls", NULL);
 
-    else { 
+    } else { 
         /* padre */
         wait(NULL);
-        printf("Child Complete");
+        printf("Soy un proceso padre, mi PID es %d y el PID de mi hijo es %d \n", getpid(), pid);
         exit(0);
     }
 }
